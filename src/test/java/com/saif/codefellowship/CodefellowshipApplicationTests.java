e147b1285eedb52391a359fd079b8881eaf49f1e
saifyahya <saifbarakat991@gmail.com>
2023-09-17 13:44:19 +0000

    first comment

---
+++ src/test/java/com/saif/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saif.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
